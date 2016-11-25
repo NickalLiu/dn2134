5e0b57dbc0014999cdfc348f606e2066c908d97d
284848144@qq.com <284848144@qq.com>
2016-11-25 16:07:26 +0800

    v1

--- dn2134/urls.py
+++ dn2134/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from main.views import exam, wechat_main
+
 
 urlpatterns = [
     # Examples:
@@ -7,4 +9,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+	url(r'^exam/', exam),
+	url(r'^wechat/', wechat_main),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+ 
+from django.http.response import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render_to_response
+ 
+from wechat_sdk import WechatBasic
+from wechat_sdk.exceptions import ParseError
+from wechat_sdk.messages import TextMessage
+ 
+ 
+WECHAT_TOKEN = '09231211'
+AppID = 'wx693c4716eeb8db16'
+AppSecret = '7338069c1463b40d3f81f3b814133668'
+ 
+# 实例化 WechatBasic
+wechat_instance = WechatBasic(
+    token=WECHAT_TOKEN,
+    appid=AppID,
+    appsecret=AppSecret
+)
 
-# Create your views here.
+@csrf_exempt
+def exam(request):
+    return render_to_response(request, 'index.html')
+
+@csrf_exempt
+def weixin_main(request):
+    if request.method == 'GET':
+        # 检验合法性
+        # 从 request 中提取基本信息 (signature, timestamp, nonce, xml)
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+ 
+        if not wechat_instance.check_signature(
+                signature=signature, timestamp=timestamp, nonce=nonce):
+            return HttpResponseBadRequest('Verify Failed')
+ 
+        return HttpResponse(
+            request.GET.get('echostr', ''), content_type="text/plain")
+ 
+ 
+    # 解析本次请求的 XML 数据
+    try:
+        wechat_instance.parse_data(data=request.body)
+    except ParseError:
+        return HttpResponseBadRequest('Invalid XML Data')
+ 
+    # 获取解析好的微信请求信息
+    message = wechat_instance.get_message()
+ 
+    # 关注事件以及不匹配时的默认回复
+    response = wechat_instance.response_text(
+        content = (
+            '感谢您的关注！\n目前支持的回复内容：\n复习\n'
+                    '考试\n'
+            ))
+    if isinstance(message, TextMessage):
+        # 当前会话内容
+        content = message.content.strip()
+        if content == '复习':
+            reply_text = '<a href="http://www.ziqiangxuetang.com">点击进入【全题复习】</a>'
+
+        elif content == '考试':
+            reply_text = '<a href="http://111.202.44.94/exam/">点击进入【模拟测试】</a>'
+
+        else :
+            reply_text = '回复内容无效，请重新输入。'
+ 
+        response = wechat_instance.response_text(content=reply_text)
+ 
+    return HttpResponse(response, content_type="application/xml")
