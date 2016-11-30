6e5631ee18ea30d6a661a62652883d36e2003efe
liuhaodong <284848144@qq.com>
2016-11-30 16:42:57 +0800

    k2

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 # Create your models here.
+
+class Exam(models.Model):
+    title = models.CharField(max_length=30)
+    body = models.CharField(max_length=30)
