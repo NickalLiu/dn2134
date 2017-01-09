b155d38292aaab43e2b14046aa831cdbc8446be0
NickalLiu <284848144@qq.com>
2017-01-10 01:45:52 +0800

    lslslowl

--- main/views.py
+++ main/views.py
@@ -24,10 +24,10 @@ wechat_instance = WechatBasic(
 
 @csrf_exempt
 def exam(request):
-    fs = file(r"/opt/dn2134/s.jo")
-    jsonBuf = json.load(fs)
-    data = jsonBuf["questions"][1]
-    return render_to_response('test.html', {"data" : data})
+    #fs = file(r"/opt/dn2134/exam.json")
+    fs = file(r"C:/Users/Administrator/Desktop/git/dn2134/exam.json")
+    data = json.load(fs)
+    return render_to_response('test.html', {"qst" : data["questions"], "ans" : data["answers"]})
 
 @csrf_exempt
 def index(request):
